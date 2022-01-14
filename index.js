4f07a76e700c35ed526c0a89932fb8def57c75f7
Sharon Odima <odimasharon@gmail.com>
2022-01-14 07:05:06 -0500

    Sharon odima completed Module 4 Project Task 5

--- index.js
+++ index.js
@@ -105,7 +105,8 @@ Write a function that creates an object with name, rating, feedback, add the new
   4. should return the resulting array
 */
 
-function addReview(array, newObject){
+function addReview(array, name, rating, feedback){
+  const newObject = {name: name, rating: rating, feedback:  feedback}
   array.push(newObject);
   return array;
 }
@@ -175,7 +176,7 @@ Use the getLongReviews function below to do the following:
 
   For example: getLongReviews(reviews) would return:
   [
-    {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+    {name: "Wen", rating: 4.5,sfeedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }
   ]
